8a7fcf5a4f51dbf6f73df96320fc39a5eaf396a1
Brian Woodward <brian.woodward@gmail.com>
2015-08-06 02:13:19 -0400

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * two <https://github.com/beepbop/two>
+ *
+ * Copyright (c) 2015, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
